9e4f5bb18d68f36d0cd78b4e8cc208ff67e0df1a
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-08-09 20:54:28 +0545

    Hello world 1.2

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 
 <?php
 $name = array("Mingmar","Gyalzen","Sherpa");
-$firstName = "Mingmar";
-$middleName = "Gyalzen";
-$lastName = "Sherpa";
 echo "Hello World, I am ";
 foreach($name as $word){
     if($word == $name[2]) echo "$word.";
